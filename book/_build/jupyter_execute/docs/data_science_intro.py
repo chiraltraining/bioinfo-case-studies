a91c1d0a6ecd6d04cf4acd2a08ecffd97fea47f8
Jubayer Hossain <work.jubayer@gmail.com>
2020-07-01 16:29:22 +0600

    initial

---
+++ book/_build/jupyter_execute/docs/data_science_intro.py
@@ -0,0 +1,12 @@
+# Data Science Using Python
+
+## Import Libraries
+
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+x = np.linspace(0, 10, 50)
+y = x**2 
+plt.plot(y) 
+plt.show() 
